ff9d612d2b9658db992e8844b1738461f8f4b600
Glenda <glendahong2013@gmail.com>
2021-09-30 14:30:56 +0800

    edited DatabaseCtrl.dart

--- lib/src/control/DatabaseCtrl.dart
+++ lib/src/control/DatabaseCtrl.dart
@@ -2,38 +2,45 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../entity.dart';
+
 class DatabaseCtrl{
-  final String carparkInfoName = 'hdb-carpark-information';
-  final String carparkInfoUrl = 'https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c';
+  final String _carparkInfoUrl = 'https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c';
+
+  final CollectionReference carparkInfoCollection = FirebaseFirestore.instance
+      .collection('hdb-carpark-information')
+      .withConverter(
+        fromFirestore: (snapshot, _) => CarparkDataHandler.fromJson(snapshot.data() as Map),
+        toFirestore: (object, _) => (object as CarparkDataHandler).toJson()
+      );
 
   DatabaseCtrl();
 
+  getCarpark(String carparkNo) => carparkInfoCollection.doc(carparkNo).get().then((carpark) => carpark);
+
   updateCarparkInfo() async{
     // Query data
-    String ckanQuery = carparkInfoUrl;
+    String ckanQuery = _carparkInfoUrl;
     http.Response response = await http.get(Uri.parse(ckanQuery));
     Map result = json.decode(response.body)['result'];
 
     // If dataset size exceeds the default query limit (100), query again
     int limit = result['total'];
     if (limit > 100){
-      ckanQuery = '$carparkInfoUrl&limit=$limit';
+      ckanQuery = '$_carparkInfoUrl&limit=$limit';
       http.Response response = await http.get(Uri.parse(ckanQuery));
       result = json.decode(response.body)['result'];
     }
 
     // Update database
     List data = result['records']; // Extract data from result
-    CollectionReference carparkInfoCollection = FirebaseFirestore.instance.collection(carparkInfoName);
     for (var i=0; i<data.length; i++){
       // Format data
-      Map carparkData = data[i];
-      String carparkNo = carparkData.remove('car_park_no');
-      carparkData.remove('_id'); // strip id field from data
+      CarparkDataHandler carpark = CarparkDataHandler.fromJson(data[i]);
 
       // if document exists in collection, update document
       // else, add document
-      carparkInfoCollection.doc(carparkNo).set(carparkData);
+      carparkInfoCollection.doc(carpark.id).set(carpark);
     }
   }
 }
