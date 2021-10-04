0b5f3eb435c7eb9bad2dd825a97db14a95821fa0
weehsuan <74612522+weehsuan@users.noreply.github.com>
2021-10-05 01:15:20 +0800

    Updated the bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
+import 'package:car_park_app/entities/all.dart';
+import 'package:car_park_app/pages/services/auth.dart';
+import 'package:car_park_app/pages/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:car_park_app/pages/BookingUI.dart';
 import 'package:car_park_app/pages/FavouritesUI.dart';
 import 'package:car_park_app/pages/ForgotPasswordUI.dart';
 import 'package:car_park_app/pages/HistoryUI.dart';
 import 'package:car_park_app/pages/HomeUI.dart';
-import 'package:car_park_app/pages/LoginUI.dart';
+import 'package:car_park_app/pages/authenticate/LoginUI.dart';
 import 'package:car_park_app/pages/MapUI.dart';
 import 'package:car_park_app/pages/MyAccountUI.dart';
 import 'package:car_park_app/pages/RegistrationUI.dart';
 import 'package:car_park_app/pages/SearchResultUI.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Car Park App',
-      home: myBottomNavigationBar(),
+    return StreamProvider<UserAccount?>.value(
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        title: 'Car Park App',
+        home: myBottomNavigationBar(),
+      ),
+      catchError: (User, UserAccount) => null,
     );
   }
 }
