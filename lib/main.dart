902895e103a88cf53d4333c4866ca44562456200
weehsuan <74612522+weehsuan@users.noreply.github.com>
2021-10-04 20:34:41 +0800

    Added the bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,80 @@
-import 'package:car_park_app/entities/all.dart';
-import 'package:car_park_app/pages/services/auth.dart';
-import 'package:car_park_app/pages/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:car_park_app/pages/BookingUI.dart';
 import 'package:car_park_app/pages/FavouritesUI.dart';
 import 'package:car_park_app/pages/ForgotPasswordUI.dart';
 import 'package:car_park_app/pages/HistoryUI.dart';
 import 'package:car_park_app/pages/HomeUI.dart';
-import 'package:car_park_app/pages/authenticate/LoginUI.dart';
+import 'package:car_park_app/pages/LoginUI.dart';
 import 'package:car_park_app/pages/MapUI.dart';
 import 'package:car_park_app/pages/MyAccountUI.dart';
 import 'package:car_park_app/pages/RegistrationUI.dart';
 import 'package:car_park_app/pages/SearchResultUI.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<UserAccount?>.value(
-      initialData: null,
-      value: AuthService().user,
-      child: MaterialApp(
-        home: Wrapper(),
-        title: 'Car Park App',
-        initialRoute: '/wrapper',
-        routes: {
-          //'/': (context) => LoginUI(),
-          '/wrapper': (context) => Wrapper(),
-          '/booking': (context) => BookingUI(),
-          '/favourites': (context) => FavouritesUI(),
-          '/forgotpassword': (context) => ForgotPasswordUI(),
-          '/history': (context) => HistoryUI(),
-          '/home': (context) => HomeUI(),
-          '/map': (context) => MapUI(),
-          '/myaccount': (context) => MyAccountUI(),
-          '/registration': (context) => RegistrationUI(),
-          '/searchresult': (context) => SearchResultUI(),
-        },
+    return MaterialApp(
+      title: 'Car Park App',
+      home: myBottomNavigationBar(),
+    );
+  }
+}
+
+class myBottomNavigationBar extends StatefulWidget {
+  @override
+  _myBottomNavigationBarState createState() => _myBottomNavigationBarState();
+}
+
+class _myBottomNavigationBarState extends State<myBottomNavigationBar> {
+  int _selectedIndex = 0;
+  final List<Widget> children = [
+    HomeUI(),
+    BookingUI(),
+    HistoryUI(),
+    FavouritesUI(),
+    MyAccountUI(),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_car_filled),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline_outlined),
+            label: 'Favourites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Account',
+          ),
+        ],
       ),
-      catchError: (User, UserAccount) => null,
     );
   }
 }
