a362bcc8acac6b19401ee4ad197f378e0850655c
Joey Lim <joey.limjiayi@icloud.com>
2021-10-12 16:07:36 +0800

    updated mapui

--- lib/pages/MapUI.dart
+++ lib/pages/MapUI.dart
@@ -1,5 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:car_park_app/entities/all.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
+import '../control/DatabaseCtrl.dart';
 
 class MapUI extends StatefulWidget {
   const MapUI({Key? key}) : super(key: key);
@@ -8,39 +17,104 @@ class MapUI extends StatefulWidget {
 }
 
 class _MapUIState extends State<MapUI> {
+  late LatLng userPosition;
+  List<Marker> markers = [];
+
+  late LocationData _currentPosition;
+  late String _address, _dateTime;
+
+
   static const _initialCameraPosition = CameraPosition(
-    target: LatLng(1.3521, 103.8198),
-    zoom: 11
+      target: LatLng(1.3521, 103.8198),
+      zoom: 15
   );
 
   GoogleMapController? _googleMapController;
 
   @override
-  void dispose(){
+  void dispose() {
     _googleMapController?.dispose();
     super.dispose();
   }
 
+  // gets user's location
+  Future<LatLng> findUserLocation() async{
+    /*
+    Location location = Location();
+    LocationData userLocation;
+    PermissionStatus hasPermission = await
+    location.hasPermission();
+    bool active = await location.serviceEnabled();
+    if (hasPermission == PermissionStatus.granted && active){
+      userLocation = await location.getLocation();
+      userPosition = LatLng(userLocation!.latitude!, userLocation!.longitude!);
+    } else{
+      userPosition = LatLng(1.3521, 103.8198);
+    } */
+    return userPosition;
+  }
+
+  Future findPlaces() async{
+    //query database
+    await Firebase.initializeApp();
+    List<Carpark> carparklist = await DatabaseCtrl.getAllCarparks();
+    showMarkers(carparklist);
+  }
+
+   showMarkers(carparklist) {
+    for (int i = 0; i < carparklist.length; i++)
+      markers.add(Marker(
+        markerId: MarkerId(carparklist[i].address),
+        position: LatLng(carparklist[i].location!.latitude,
+            carparklist[i].location!.longitude),
+        infoWindow:
+          InfoWindow(title: "address", snippet: "address")));
+    setState((){
+      markers = markers;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Map'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-      ),
-      body: GoogleMap(
-        initialCameraPosition: _initialCameraPosition,
-        onMapCreated: (controller) => _googleMapController = controller,
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).primaryColor,
-        foregroundColor: Colors.black,
-        onPressed: () => _googleMapController!.animateCamera(
-          CameraUpdate.newCameraPosition(_initialCameraPosition),
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Map'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          actions: [
+            IconButton(
+              icon: Icon(Icons.map),
+              onPressed: () => findPlaces(),
+            )
+          ]
         ),
-        child: const Icon(Icons.center_focus_strong),
-      )
-    );
-  }
+
+        body: FutureBuilder(
+          future: findUserLocation(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            return GoogleMap(
+              initialCameraPosition: _initialCameraPosition,
+              onMapCreated: (controller) => _googleMapController = controller,
+              myLocationButtonEnabled: true,
+              markers: Set<Marker>.of(markers),
+            );
+          },
+        ),
+
+
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).primaryColor,
+          foregroundColor: Colors.black,
+          onPressed: () => _googleMapController!.animateCamera(
+            CameraUpdate.newCameraPosition(_initialCameraPosition),
+          ),
+          child: const Icon(Icons.center_focus_strong),
+        )
+      );
+    }
 }
+
+
+
+
+
