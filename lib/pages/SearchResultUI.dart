b70f933452944775a5c981613317c6179bb9fc06
Devansh Koppar <devanshk22@gmail.com>
2021-10-26 18:19:31 +0800

    Create Search page

--- lib/pages/SearchResultUI.dart
+++ lib/pages/SearchResultUI.dart
@@ -3,6 +3,8 @@ import 'package:car_park_app/utilities/locationMgr.dart';
 import 'package:flutter/material.dart';
 import 'package:car_park_app/constants/app_constants.dart';
 import 'package:car_park_app/widgets/CarparkCard.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:car_park_app/widgets/searchResultListView.dart';
 
 class SearchResultUI extends StatefulWidget {
   const SearchResultUI({Key? key}) : super(key: key);
@@ -12,108 +14,184 @@ class SearchResultUI extends StatefulWidget {
 }
 
 class _SearchResultUIState extends State<SearchResultUI> {
+  static const historyLength = 5;
+
+  // The "raw" history that we don't access from the UI
+  List<String> _searchHistory = [];
+// The filtered & ordered history that's accessed from the UI
+  List<String> filteredSearchHistory = [];
+
+// The currently searched-for term
+  String selectedTerm = "";
+
+  FloatingSearchBarScrollNotifier searchBarResults =
+      new FloatingSearchBarScrollNotifier(
+    child: SearchResultsListView(
+      searchTerm: "",
+    ),
+  );
+
+  List<String> filterSearchTerms({
+    required String filter,
+  }) {
+    if (filter.isNotEmpty) {
+      // Reversed because we want the last added items to appear first in the UI
+      return _searchHistory.reversed
+          .where((term) => term.startsWith(filter))
+          .toList();
+    } else {
+      return _searchHistory.reversed.toList();
+    }
+  }
+
+  void addSearchTerm(String term) {
+    if (_searchHistory.contains(term)) {
+      // This method will be implemented soon
+      putSearchTermFirst(term);
+      return;
+    }
+    _searchHistory.add(term);
+    if (_searchHistory.length > historyLength) {
+      _searchHistory.removeRange(0, _searchHistory.length - historyLength);
+    }
+    // Changes in _searchHistory mean that we have to update the filteredSearchHistory
+    filteredSearchHistory = filterSearchTerms(filter: "");
+  }
+
+  void deleteSearchTerm(String term) {
+    _searchHistory.removeWhere((t) => t == term);
+    filteredSearchHistory = filterSearchTerms(filter: "");
+  }
+
+  void putSearchTermFirst(String term) {
+    deleteSearchTerm(term);
+    addSearchTerm(term);
+  }
+
+  late FloatingSearchBarController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FloatingSearchBarController();
+    filteredSearchHistory = filterSearchTerms(filter: "");
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text('Search'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
       ),
-      body: FutureBuilder(
-        future: Future.wait([getNearbyCarpark(), getPosition()]),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.done)
-            return SingleChildScrollView(
-              physics: ScrollPhysics(),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.bottomLeft,
-                    padding: EdgeInsets.fromLTRB(
-                        1.5 * screenGap, 2 * cardGap, 1.5 * screenGap, cardGap),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Text(
-                            'Nearby Favourites',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Roboto',
-                                fontSize: 20,
-                                letterSpacing:
-                                    0 /*percentages not used in flutter. defaulting to zero*/,
-                                fontWeight: FontWeight.normal,
-                                height: 1),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: TextButton.icon(
-                            onPressed: () {
-                              setState(() {});
-                            },
-                            icon: Icon(
-                              Icons.refresh,
-                              color: Colors.white,
-                            ),
-                            label: Text(
-                              'Refresh',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
+      body: FloatingSearchBar(
+        margins: EdgeInsets.all(18.0),
+        automaticallyImplyBackButton: false,
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Material(
+              color: Colors.white,
+              elevation: 4,
+              child: Builder(
+                builder: (context) {
+                  if (filteredSearchHistory.isEmpty &&
+                      controller.query.isEmpty) {
+                    return Container(
+                      height: 56,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Start searching',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    );
+                  } else if (filteredSearchHistory.isEmpty) {
+                    return ListTile(
+                      title: Text(controller.query),
+                      leading: const Icon(Icons.search),
+                      onTap: () {
+                        setState(() {
+                          addSearchTerm(controller.query);
+                          selectedTerm = controller.query;
+                        });
+                        controller.close();
+                      },
+                    );
+                  } else {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: filteredSearchHistory
+                          .map(
+                            (term) => ListTile(
+                              title: Text(
+                                term,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.lightBlue),
+                              leading: const Icon(Icons.history),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    deleteSearchTerm(term);
+                                  });
+                                },
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  putSearchTermFirst(term);
+                                  selectedTerm = term;
+                                });
+                                controller.close();
+                              },
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data[0].length,
-                      itemBuilder: (context, index) {
-                        return FutureBuilder(
-                          future: checkFavourite(snapshot.data[0][index]),
-                          builder:
-                              (BuildContext context, AsyncSnapshot snapshot2) {
-                            if (snapshot2.connectionState ==
-                                ConnectionState
-                                    .done) if (snapshot2.data == true)
-                              return Container(
-                                padding: EdgeInsets.fromLTRB(
-                                    screenGap, cardGap, screenGap, cardGap),
-                                child: CarparkCard(
-                                  carpark: snapshot.data[0][index],
-                                  lat: snapshot.data[1].latitude,
-                                  lng: snapshot.data[1].longitude,
-                                  isFavourite: snapshot2.data,
-                                ),
-                              );
-                            else
-                              return SizedBox.shrink();
-                            else
-                              return SizedBox.shrink();
-                          },
-                        );
-                      }),
-                ],
+                          )
+                          .toList(),
+                    );
+                  }
+                },
+              ),
+            ),
+          );
+        },
+        transition: CircularFloatingSearchBarTransition(),
+        // Bouncing physics for the search history
+        physics: BouncingScrollPhysics(),
+        hint: "Search for Carpark",
+        actions: [
+          FloatingSearchBarAction.searchToClear(),
+        ],
+        onQueryChanged: (query) {
+          setState(() {
+            filteredSearchHistory = filterSearchTerms(filter: query);
+          });
+        },
+        onSubmitted: (query) {
+          setState(() {
+            addSearchTerm(query);
+            selectedTerm = query;
+            searchBarResults = FloatingSearchBarScrollNotifier(
+              child: SearchResultsListView(
+                searchTerm: selectedTerm,
               ),
             );
-          else
-            return Center(child: CircularProgressIndicator());
+          });
+          controller.close();
         },
+        controller: controller,
+        body: searchBarResults,
       ),
     );
   }
