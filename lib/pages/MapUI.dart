7d2fd97826b72b88f1255ebf94fa637e3713c5b0
Joey Lim <joey.limjiayi@icloud.com>
2021-11-01 19:17:14 +0800

    added the carpark informatoin screen

--- lib/pages/MapUI.dart
+++ lib/pages/MapUI.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
+
 import 'package:car_park_app/entities/all.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:car_park_app/pages/CarparkInformationScreenUI.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import '../control/DatabaseCtrl.dart';
+
 
 class MapUI extends StatefulWidget {
   const MapUI({Key? key}) : super(key: key);
@@ -13,12 +17,12 @@ class MapUI extends StatefulWidget {
 }
 
 class _MapUIState extends State<MapUI> {
-  late LatLng userPosition;
   List<Marker> markers = [];
 
-  Location _location = Location();
+  Location _location = new Location();
+
   static const _initialCameraPosition =
-      CameraPosition(target: LatLng(1.3521, 103.8198), zoom: 15);
+    CameraPosition(target: LatLng(1.3521, 103.8198), zoom: 15);
 
   GoogleMapController? _googleMapController;
 
@@ -32,12 +36,35 @@ class _MapUIState extends State<MapUI> {
         ),
       );
     });
+    findPlaces();
   }
 
   @override
   void dispose() {
     _googleMapController?.dispose();
     super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Map'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+        ),
+
+        body: FutureBuilder(
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return GoogleMap(
+              initialCameraPosition: _initialCameraPosition,
+              onMapCreated: _onMapCreated,
+              myLocationButtonEnabled: false,
+              markers: Set<Marker>.of(markers),
+            );
+          },
+        ),
+    );
   }
 
   Future findPlaces() async {
@@ -48,48 +75,37 @@ class _MapUIState extends State<MapUI> {
     showMarkers(carparklist);
   }
 
-  showMarkers(carparklist) {
-    for (int i = 0; i < carparklist.length; i++)
-      markers.add(Marker(
-          markerId: MarkerId(carparklist[i].address),
+  showMarkers(carparklist) async {
+    Position _currentLocation = await Geolocator.getCurrentPosition();
+
+    for (int i = 0; i < carparklist.length; i++) {
+      double _distanceBetween = (Geolocator.distanceBetween(_currentLocation.latitude, _currentLocation.longitude, carparklist[i].location.latitude, carparklist[i].location.longitude)) / 1000;
+
+          markers.add(Marker(
+          markerId: MarkerId(i.toString()),
           position: LatLng(carparklist[i].location!.latitude,
               carparklist[i].location!.longitude),
-          infoWindow: InfoWindow(title: "address", snippet: "address")));
-    setState(() {
-      markers = markers;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-            title: Text('Map'),
-            centerTitle: true,
-            backgroundColor: Colors.grey[850],
-            actions: [
-              IconButton(
-                icon: Icon(Icons.map),
-                onPressed: () => findPlaces(),
-              )
-            ]),
-        body: FutureBuilder(
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return GoogleMap(
-              initialCameraPosition: _initialCameraPosition,
-              onMapCreated: _onMapCreated,
-              myLocationButtonEnabled: true,
-              markers: Set<Marker>.of(markers),
+          infoWindow:
+          InfoWindow(title: carparklist[i].address,
+            ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                new CarparkInformationScreenUI(onecarpark: carparklist[i], distanceBetween: _distanceBetween),
+              ),
             );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: Colors.black,
-          onPressed: () => _googleMapController!.animateCamera(
-            CameraUpdate.newCameraPosition(_initialCameraPosition),
-          ),
-          child: const Icon(Icons.center_focus_strong),
-        ));
+          }));
+      setState(() {
+        markers = markers;
+      });
+
+    }
   }
 }
+
+
+
+
+
