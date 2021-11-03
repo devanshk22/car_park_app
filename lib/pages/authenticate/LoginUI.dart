96c63c09b84a1aedc385f372ab1ea94e5bc9a4f3
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2021-11-03 21:23:01 +0800

    change label in LoginUI from Email ID to Email

--- lib/pages/authenticate/LoginUI.dart
+++ lib/pages/authenticate/LoginUI.dart
@@ -47,8 +47,8 @@ class _LoginUIState extends State<LoginUI> {
               ),
               TextFormField(
                 decoration: textInputDecoration.copyWith(
-                  hintText: 'Email ID',
-                  labelText: 'Email ID',
+                  hintText: 'Email',
+                  labelText: 'Email',
                   labelStyle: TextStyle(color: Colors.pink, fontSize: 16.0),
                   prefixIcon: Icon(
                     Icons.email,
@@ -56,7 +56,7 @@ class _LoginUIState extends State<LoginUI> {
                   ),
                 ),
                 validator: (val) =>
-                    val!.isEmpty ? "Please enter an Email ID" : null,
+                    val!.isEmpty ? "Please enter an Email" : null,
                 onChanged: (value) {
                   setState(() => email = value);
                 },
